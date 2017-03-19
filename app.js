932088808477ce17cdc4b99674b69a3c85fae5ef
Michał Tukalski <michaltukalski@gmail.com>
2017-03-19 10:46:50 +0100

    identyfikowanie typow referncyjnych

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-literał obiektów
+//literał obiektów
 var car ={
   manufacturer: "Ford",
   model: "Mondeo"
@@ -13,5 +13,36 @@ var car2;
 car2.manufacturer = "Fiat";
 car2.model = 500;
 
-literał tablic
+//literał tablic
 var arr=["blue", "green", "yellow"];
+
+//literał wyrażeń regularnych
+
+var numbers = /\d+/g;
+
+var numbers2 = RegExp('\\d+', 'g');
+
+//dostęp do kluczy
+
+var arr = [];
+arr.push(3333
+
+var method = 'push';
+array[method](3453);
+
+//identyfikowanie typów referwncyjnych
+
+function add(a,b){
+  return a+b;
+}
+(typeof add) //function
+
+var item = [];
+var obj = {};
+
+console.log(item instanceof Array); //true
+console.log(item instanceof Object); //true
+console.log(obj instanceof Object); //true
+console.log(obj instanceof Array); //false
+console.log(add instanceof Function); //true
+console.log(add instanceof Object); //true
