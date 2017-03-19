558ce659de0f11c138d14a823cc9eb9ec7f00436
Michał Tukalski <michaltukalski@gmail.com>
2017-03-19 09:54:42 +0100

    literal obiektow

---
+++ app.js
@@ -0,0 +1,17 @@
+literał obiektów
+var car ={
+  manufacturer: "Ford",
+  model: "Mondeo"
+};
+
+var car1 = {
+  'manufacturer origin':"Ford",
+  model: 'Mondeo'
+};
+
+var car2;
+car2.manufacturer = "Fiat";
+car2.model = 500;
+
+literał tablic
+var arr=["blue", "green", "yellow"];
