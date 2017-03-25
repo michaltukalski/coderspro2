4c027af78da1dce6c0434773233cc5eb20673550
Michał Tukalski <michaltukalski@gmail.com>
2017-03-25 09:27:20 +0100

    weekend 6 day1 - powtorka

---
+++ app2.js
@@ -0,0 +1,16 @@
+//dzień pierwszy weekendu 6.
+//powtórka z zeszłego tygodnia
+
+//definiowanie właściwości
+
+var person = {
+  name: 'kuba'
+}
+
+person.age = 11;
+
+//[[Put]] wewnętrzna metoda JS do tworzenia nowych właściwości
+//[[Set]] wewnętrzna metoda JS do nadpisywania istniejących właściwości
+
+person.name = 'Bartek';
+console.log (person.name); //Bartek
